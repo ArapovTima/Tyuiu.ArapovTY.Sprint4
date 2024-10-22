47540581d092ceba5ae53b6f0d8c2a694d490d98
Тимофей Арапов <Tyuiu.ArapovTY@outlook.com>
2024-10-22 16:44:24 +0500

    Task1

---
+++ Tyuiu.ArapovTY.Sprint4.Task1.V3/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.ArapovTY.Sprint4.Task1.V3.Lib;
+namespace Tyuiu.ArapovTY.Sprint4.Task1.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Арапов Т. Я. | АСОиУб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы (ввод с клавиатуры)                            *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Арапов Тимофей Яковлевич | АСОиУб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Подсчитать произведение чётных элементов массива.                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int len;
+            Console.WriteLine("Введите количество элементов массива: ");
+            len = Convert.ToInt32(Console.ReadLine());
+            int[] numsArry = new int[len];
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("Введите значение " + i + " элемента массива: ");
+                numsArry[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Массив: ");
+            for (int i = 0;i <= len - 1 ;i++)
+            {
+                Console.Write(numsArry[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(numsArry);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
